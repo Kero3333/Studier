d1e53a3c28c2d3d62b72321b91db50e54d93c44e
Kévin Robic <kevin.r@institutsolacroup.com>
2022-04-07 14:00:05 +0200

    login/front

--- back/server.js
+++ back/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = 3000 || process.env.PORT;
 
+app.use(express.json());
+
 const auth = require("./routes/auth");
+
+app.use(cors());
 
 app.use("/auth", auth);
 
