1fd296979d5f476d81f686ba9d67ac5784c4c585
Kévin Robic <kevin.r@institutsolacroup.com>
2022-04-07 13:06:37 +0200

    register

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const axios = require("axios");
 
-router.post("", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { data } = await axios.post(
       "https://strapi3333.herokuapp.com/api/auth/local",
@@ -10,6 +10,18 @@ router.post("", async (req, res) => {
     res.send(data);
   } catch (err) {
     res.status(400).send("wrong email or password");
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { data } = await axios.post(
+      "https://strapi3333.herokuapp.com/api/auth/local/register",
+      req.body
+    );
+    res.send(data);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 });
 
