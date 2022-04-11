c559b86d45dec2648b499f86aa5abedeee4a6821
dylanbourdais <dylan.bo@institutsolacroup.com>
2022-04-11 15:47:36 +0200

    img

--- back/server.js
+++ back/server.js
@@ -8,10 +8,13 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.json());
 
 const auth = require("./routes/auth");
+const covoiturage = require("./routes/covoiturage");
 
 app.use(cors());
 
 app.use("/auth", auth);
+
+app.use("/covoiturage", covoiturage);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
