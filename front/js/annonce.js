1298a04780711338a3c17ccff71dbd8892eb3b49
Kévin Robic <kevin.r@institutsolacroup.com>
2022-04-14 14:50:24 +0200

    event announces

--- front/js/annonce.js
+++ front/js/annonce.js
@@ -1,58 +1,39 @@
-/* <div class="item">
-<div class="img-details">
-    <img  src="https://source.unsplash.com/random">
-     <img class="favorite-item" src="./img/heart.jpg" alt="favorite-icon">
-</div>
-<div class="flex-card">
-    <img class="logo-profil" src="http://www.pixeden.com/media/k2/galleries/468/001-business-card-clip-brand-mock-up-vol-20-psd.jpg" alt="logo">
-    <div class="flex-column">
-        <h4>Titre</h4>
-        <p> Lorem ipsum dolor sit, amet consectetur</p>
-    </div>
-</div>
-</div> */
+const axios = require("axios");
 
+async function element() {
+  const { data } = await axios.get(
+    `https://strapi3333.herokuapp.com/api/announces?populate=*`
+  );
 
+  const results = data.data;
 
+  const masonry = document.querySelector(".masonry");
 
-const axios = require('axios');
+  results.forEach((element) => {
+    const div = document.createElement("div");
+    div.className = "item";
 
-async function element(){
+    div.innerHTML += `
+        <div class="img-details">
+        <img  src="${element.attributes.picture}">
+        <img class="favorite-item" src="../img/heart.jpg" alt="favorite-icon">
+        </div>
+        <div class="flex-card">
+            <img class="logo-profil" src="${element.attributes.user.data.attributes.picture_profile}" alt="logo">
+            <div class="flex-column">
+                <h4> ${element.attributes.title}</h4>
+                <p> ${element.attributes.description}</p>
+            </div>
+        </div>
+    `;
 
-    const {data} = await axios.get(`https://strapi3333.herokuapp.com/api/announces?populate=*`);
+    div.addEventListener("click", () => {
+      localStorage.setItem("announce", element.id);
+      document.location.href = "description.html";
+    });
 
-    const results = data.data;
+    masonry.appendChild(div);
+  });
+}
 
-    const div = document.querySelector('.masonry');
-
-   results.forEach(element => {
-
-     div.innerHTML += `
-     <div class="item">
-    <div class="img-details">
-    <img  src="${element.attributes.picture}">
-    <img class="favorite-item" src="../img/heart.jpg" alt="favorite-icon">
-    </div>
-<div class="flex-card">
-    <img class="logo-profil" src="${element.attributes.user.data.attributes.picture_profile}" alt="logo">
-    <div class="flex-column">
-        <h4> ${element.attributes.title}</h4>
-        <p> ${element.attributes.description}</p>
-    </div>
-</div>
-</div> 
-`;
-
-console.log(element.attributes);
-
-   });
-
-};
-
- element(); 
-
-    
-  
-
-
-
+element();
