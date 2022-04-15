f0fe207915bc091b8a165be4ac91f20b6efab523
Kévin Robic <kevin.r@institutsolacroup.com>
2022-04-15 11:26:30 +0200

    favori

--- front/js/favori.js
+++ front/js/favori.js
@@ -57,45 +57,45 @@ async function element() {
 
   const id = data.id;
 
-  console.log(id);
-
   let two = `https://strapi3333.herokuapp.com/api/favoris?populate=announces_ids&filters[users_id]=${id}`;
 
   const { data: favori } = await axios.get(two, {
     headers: { Authorization: "Bearer " + localStorage.getItem("token") },
   });
 
-  console.log(data);
-
   const results = favori.data[0].attributes.announces_ids.data.map(
     (announce) => {
-      return announce.attributes;
+      return announce;
     }
   );
 
-  const div = document.querySelector(".masonry");
+  const masonry = document.querySelector(".masonry");
 
   results.forEach((element) => {
-    console.log(element);
+    const div = document.createElement("div");
+    div.className = "item";
 
     div.innerHTML += `
-       <div class="item">
-       <div class="img-details">
-       <img  src="${element.picture}">
-       <img class="favorite-item" src="https://res.cloudinary.com/dvpi39ag2/image/upload/v1649853368/img/heart_gerq0k.png" alt="favorite-icon">
-       </div>
-   <div class="flex-card">
-       <div class="flex-column">
-           <h4> ${element.title}</h4>
-           <p> ${element.description}</p>
-           <p>${element.number_of_like} :D</p>
-       </div>
-   </div>
-   </div> 
-        `;
-  });
+        <div class="img-details">
+        <img  src="${element.attributes.picture}">
+        <img class="favorite-item" src="https://res.cloudinary.com/dvpi39ag2/image/upload/v1649853368/img/heart_gerq0k.png" alt="favorite-icon">
+        </div>
+        <div class="flex-card">
+            <div class="flex-column">
+                <h4> ${element.attributes.title}</h4>
+                <p> ${element.attributes.description}</p>
+                <p> ${element.attributes.number_of_like} :D</p>
+            </div>
+        </div>
+    `;
 
-  console.log(results);
+    div.addEventListener("click", () => {
+      localStorage.setItem("announce", element.id);
+      document.location.href = "description.html";
+    });
+
+    masonry.appendChild(div);
+  });
 }
 
 updateNbLike();
